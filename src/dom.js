116d4acae150df1eeb4e0960bbc290a1ca799bfb
IshwarGautam <ishwargautam1@gmail.com>
2022-01-01 13:14:28 +0545

    Day 2: Basic layout of Ludo is built

--- src/dom.js
+++ src/dom.js
@@ -2,21 +2,59 @@
 // Get the DOM Elements from HTML
 //=====================================
 
+// 1. Main wrapper
 const container = document.getElementById('container');
-const header = document.getElementById('header');
-const message = document.getElementById('message-box');
-const ludo = document.getElementById('ludo');
 
-const left_part = document.getElementById('left-part');
+// 2. Top, middle and bottom container
+const top_part = document.getElementById('top-part');
 const middle_part = document.getElementById('middle-part');
-const right_part = document.getElementById('right-part');
+const bottom_part = document.getElementById('bottom-part');
 
+// 3. Element of top part
 const initial_green_region = document.getElementById("initial-green-region");
-const between_green_red = document.getElementById("between-green-red");
-const initial_red_region = document.getElementById("initial-red-region");
 const between_green_yellow = document.getElementById("between-green-yellow");
-const winner_region = document.getElementById("winner-region");
-const between_red_blue = document.getElementById("between-red-blue");
 const initial_yellow_region = document.getElementById("initial-yellow-region");
+
+// 4. Element of middle part
+const between_green_red = document.getElementById("between-green-red");
+const winner_region = document.getElementById("winner-region");
 const between_yellow_blue = document.getElementById("between-yellow-blue");
+
+// 5. Element of bottom part
+const initial_red_region = document.getElementById("initial-red-region");
+const between_red_blue = document.getElementById("between-red-blue");
 const initial_blue_region = document.getElementById("initial-blue-region");
+
+// 6. Defining color boxes
+const green = document.getElementsByClassName('green');
+const yellow = document.getElementsByClassName('yellow');
+const red = document.getElementsByClassName('red');
+const blue = document.getElementsByClassName('blue');
+const white = document.getElementsByClassName('white');
+
+const green_sub_region = document.getElementById("green-sub-region");
+const yellow_sub_region = document.getElementById("yellow-sub-region");
+const red_sub_region = document.getElementById("red-sub-region");
+const blue_sub_region = document.getElementById("blue-sub-region");
+
+const green_token_area = document.getElementsByClassName("green-token-area");
+const yellow_token_area = document.getElementsByClassName("yellow-token-area");
+const red_token_area = document.getElementsByClassName("red-token-area");
+const blue_token_area = document.getElementsByClassName("blue-token-area");
+
+const green_yellow_path = document.getElementsByClassName("green-yellow-path");
+const red_blue_path = document.getElementsByClassName("red-blue-path");
+const green_red_path = document.getElementsByClassName("green-red-path");
+const yellow_blue_path = document.getElementsByClassName("yellow-blue-path");
+
+const icon = document.getElementsByClassName("icon");
+
+const triangle_container = document.getElementById("triangle-container");
+const green_final_area = document.getElementById("green-final-area");
+const red_final_area = document.getElementById("red-final-area");
+const blue_final_area = document.getElementById("blue-final-area");
+const yellow_final_area = document.getElementById("yellow-final-area");
+
+const home = document.getElementById("home");
+const text = document.getElementById("text");
+
--- src/style.js
+++ src/style.js
@@ -1,27 +1,193 @@
 //==================================================
 // Styling the Main container that wrap all elements
 //==================================================
-container.style.width = "900px";
-container.style.height = "1020px";
-container.style.border = "1px solid black";
-container.style.left = "50%";
-container.style.transform = "translate(-50%, 0%)";
-container.style.position = "absolute";
+container.style.width = "600px";
+container.style.height = "600px";
+container.style.margin = "10px auto"
 
-//==================================================================================
-// Styling the header which contains different modes of game, scores and highscore
-//==================================================================================
-header.style.height = "60px";
-header.style.borderBottom = "1px solid black";
-header.style.background = "#006400";
-header.style.color = "ivory";
+top_part.style.width = "100%";
+top_part.style.height = "40%";
+top_part.style.display = "flex";
 
-//====================================================================
-// Styling the message box that generate the information while playing
-//====================================================================
-message.style.height = "60px";
-message.style.borderBottom = "1px solid black";
-message.style.background = "#F4A460";
+middle_part.style.width = "100%";
+middle_part.style.height = "20%";
+middle_part.style.display = "flex";
+
+bottom_part.style.width = "100%";
+bottom_part.style.height = "40%";
+bottom_part.style.display = "flex";
+
+initial_green_region.style.width = "40%";
+initial_green_region.style.height = "100%";
+initial_green_region.style.alignItems = "center";
+initial_green_region.style.justifyContent = "center";
+initial_green_region.style.display = "flex";
+
+initial_yellow_region.style.width = "40%";
+initial_yellow_region.style.height = "100%";
+initial_yellow_region.style.alignItems = "center";
+initial_yellow_region.style.justifyContent = "center";
+initial_yellow_region.style.display = "flex";
+
+initial_red_region.style.width = "40%";
+initial_red_region.style.height = "100%";
+initial_red_region.style.alignItems = "center";
+initial_red_region.style.justifyContent = "center";
+initial_red_region.style.display = "flex";
+
+initial_blue_region.style.width = "40%";
+initial_blue_region.style.height = "100%";
+initial_blue_region.style.alignItems = "center";
+initial_blue_region.style.justifyContent = "center";
+initial_blue_region.style.display = "flex";
+
+between_green_yellow.style.width = "20%";
+between_green_yellow.style.height = "100%";
+
+between_red_blue.style.width = "20%";
+between_red_blue.style.height = "100%";
+
+between_green_red.style.width = "40%";
+between_green_red.style.height = "100%";
+
+between_yellow_blue.style.width = "40%";
+between_yellow_blue.style.height = "100%";
+
+winner_region.style.width = "20%";
+winner_region.style.height = "100%";
+
+// Color boxes
+for (let i=0; i<green.length; i++){
+  green[i].style.background = "#84c21f";
+  yellow[i].style.background = "#f6c700";
+  red[i].style.background = "#dc2418";
+  blue[i].style.background = "#0092dc";
+}
+
+for (let i=0; i<white.length; i++){
+  white[i].style.background = "#ffffff";
+}
+
+green_sub_region.style.width = "70%";
+green_sub_region.style.height = "70%";
+
+yellow_sub_region.style.width = "70%";
+yellow_sub_region.style.height = "70%";
+
+red_sub_region.style.width = "70%";
+red_sub_region.style.height = "70%";
+
+blue_sub_region.style.width = "70%";
+blue_sub_region.style.height = "70%";
+
+for (let i=0; i<green_token_area.length; i++){
+  green_token_area[i].style.width = "35%";
+  green_token_area[i].style.height = "35%";
+  green_token_area[i].style.margin = "7.5%";
+  green_token_area[i].style.float = "left";
+  green_token_area[i].style.boxShadow = "0 3px 6px rgba(0,0,0,0.16), 0 3px 6px rgba(0,0,0,0.23)";
+  green_token_area[i].style.transition = "all 0.3s cubic-bezier(0.25, 0.8, 25, 1)";
+
+  yellow_token_area[i].style.width = "35%";
+  yellow_token_area[i].style.height = "35%";
+  yellow_token_area[i].style.margin = "7.5%";
+  yellow_token_area[i].style.float = "left";
+  yellow_token_area[i].style.boxShadow = "0 3px 6px rgba(0,0,0,0.16), 0 3px 6px rgba(0,0,0,0.23)";
+  yellow_token_area[i].style.transition = "all 0.3s cubic-bezier(0.25, 0.8, 25, 1)";
+
+  red_token_area[i].style.width = "35%";
+  red_token_area[i].style.height = "35%";
+  red_token_area[i].style.margin = "7.5%";
+  red_token_area[i].style.float = "left";
+  red_token_area[i].style.boxShadow = "0 3px 6px rgba(0,0,0,0.16), 0 3px 6px rgba(0,0,0,0.23)";
+  red_token_area[i].style.transition = "all 0.3s cubic-bezier(0.25, 0.8, 25, 1)";
+
+  blue_token_area[i].style.width = "35%";
+  blue_token_area[i].style.height = "35%";
+  blue_token_area[i].style.margin = "7.5%";
+  blue_token_area[i].style.float = "left";
+  blue_token_area[i].style.boxShadow = "0 3px 6px rgba(0,0,0,0.16), 0 3px 6px rgba(0,0,0,0.23)";
+  blue_token_area[i].style.transition = "all 0.3s cubic-bezier(0.25, 0.8, 25, 1)";
+
+}
+
+for(let i=0; i<green_yellow_path.length; i++){
+  green_yellow_path[i].style.width = "33.33%";
+  green_yellow_path[i].style.height = "16.67%";
+  green_yellow_path[i].style.float = "left";
+  green_yellow_path[i].style.border = "1px solid black";
+
+  red_blue_path[i].style.width = "33.33%";
+  red_blue_path[i].style.height = "16.67%";
+  red_blue_path[i].style.float = "left";
+  red_blue_path[i].style.border = "1px solid black";
+
+  green_red_path[i].style.width = "16.67%";
+  green_red_path[i].style.height = "33.33%";
+  green_red_path[i].style.float = "left";
+  green_red_path[i].style.border = "1px solid black";
+
+  yellow_blue_path[i].style.width = "16.67%";
+  yellow_blue_path[i].style.height = "33.33%";
+  yellow_blue_path[i].style.float = "left";
+  yellow_blue_path[i].style.border = "1px solid black";
+}
+
+for(let i=0;i<icon.length;i++){
+  icon[i].style.margin = "3px";
+}
+
+triangle_container.style.width = "100%";
+triangle_container.style.height = "100%";
+triangle_container.style.position = "relative";
+
+green_final_area.style.width = "0";
+green_final_area.style.height = "0";
+green_final_area.style.borderTop = "55px solid transparent";
+green_final_area.style.borderLeft = "55px solid #84c21f";
+green_final_area.style.borderBottom = "55px solid transparent";
+green_final_area.style.position = "absolute";
+green_final_area.style.left = "0";
+green_final_area.style.top = "5px";
+
+red_final_area.style.width = "0";
+red_final_area.style.height = "0";
+red_final_area.style.borderLeft = "55px solid transparent";
+red_final_area.style.borderRight = "55px solid transparent";
+red_final_area.style.borderBottom = "55px solid #dc2418";
+red_final_area.style.position = "absolute";
+red_final_area.style.left = "5px";
+red_final_area.style.bottom = "0";
+
+blue_final_area.style.width = "0";
+blue_final_area.style.height = "0";
+blue_final_area.style.borderRight = "55px solid #0092dc";
+blue_final_area.style.borderBottom = "55px solid transparent";
+blue_final_area.style.borderTop = "55px solid transparent";
+blue_final_area.style.position = "absolute";
+blue_final_area.style.right = "0";
+blue_final_area.style.top = "5px";
+
+yellow_final_area.style.width = "0";
+yellow_final_area.style.height = "0";
+yellow_final_area.style.borderLeft = "55px solid transparent";
+yellow_final_area.style.borderRight = "55px solid transparent";
+yellow_final_area.style.borderTop = "55px solid #f6c700";
+yellow_final_area.style.position = "absolute";
+yellow_final_area.style.right = "5px";
+yellow_final_area.style.top = "0";
 
 
+home.style.width = "50%";
+home.style.height = "50%";
+home.style.borderRadius = "50%";
+home.style.position = "absolute";
+home.style.margin = "28px";
+home.style.background = "ivory";
+home.style.zIndex = "1";
 
+text.style.width = "80%";
+text.style.height = "40%";
+text.style.margin = "20px 5px";
+text.style.fontSize = "8px";
+text.style.fontFamily = "Georgia, 'Times New Roman', Times, serif"
