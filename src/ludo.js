1e54685135c9586b029a63f79bf7ba7b83614a12
IshwarGautam <ishwargautam1@gmail.com>
2022-01-02 20:30:55 +0545

    Day 3: Track the path for moving tokens

--- src/ludo.js
+++ src/ludo.js
@@ -1,24 +1,13 @@
-let random_num;
-let showClass;
-let currentClass = '';
-let turn = '';
-let PlayerName = [];
-let PlayerId = [0,1,2,3];
+let path = 0;
+let isOutside = 0;
 
-// Get the random number between min and max provided
-function getRandomInt(min, max) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min) + min); 
-}
-
-// Get the sound during dice rolled
-let dice_sound = new Audio('./audio/dice.mp3');
-
-function playGame(background, bottomPos, leftPos){
+function playGame(background, bottomPos, leftPos, id){
   this.background = background;
   this.bottomPos = bottomPos;
   this.leftPos = leftPos;
+  this.id = id;
+  this.path = path;
+  this.isOutside = isOutside;
 
   this.createToken = function(){
     this.token = document.createElement('div');
@@ -66,11 +55,66 @@ function playGame(background, bottomPos, leftPos){
   }
 
   this.moveToken = ()=>{
-    if (this.random_num === 0 || this.random_num === 6){
-      this.token.addEventListener("click", () => {
+    this.token.addEventListener("click", () => {
+      if (this.id === 'red' && turn === 0){
+        if (this.random_num === 0 || this.random_num === 6 || this.isOutside === 1){
+          this.isOutside = 1;
 
-      });
-    }
+          this.token.style.bottom = redBottomPath[this.path] + "px";
+          this.token.style.left = redLeftPath[this.path] + "px";
+          this.path++;
+        }
+
+        index++;
+        if (index > total_player-1) {
+          index = 0;
+        }
+        turn = PlayerId[index];
+      }
+      else if (this.id === 'green' && turn === 1){
+        if (this.random_num === 0 || this.random_num === 6 || this.isOutside === 1){
+          this.isOutside = 1;
+          this.token.style.bottom = greenBottomPath[this.path] + "px";
+          this.token.style.left = greenLeftPath[this.path] + "px";
+          this.path++;
+        }
+
+        index++;
+        if (index > total_player-1) {
+          index = 0;
+        }
+        turn = PlayerId[index];
+      }
+      else if (this.id === 'yellow' && turn === 2){
+        if (this.random_num === 0 || this.random_num === 6 || this.isOutside === 1){
+          this.isOutside = 1;
+          this.token.style.bottom = yellowBottomPath[this.path] + "px";
+          this.token.style.left = yellowLeftPath[this.path] + "px";
+          this.path++;
+        }
+
+        index++;
+        if (index > total_player-1) {
+          index = 0;
+        }
+        turn = PlayerId[index];
+      }
+      else if (this.id === 'blue' && turn === 3){
+        if (this.random_num === 0 || this.random_num === 6 || this.isOutside === 1){
+          this.isOutside = 1;
+          this.token.style.bottom = blueBottomPath[this.path] + "px";
+          this.token.style.left = blueLeftPath[this.path] + "px";
+          this.path++;
+        }
+
+        index++;
+        if (index > total_player-1) {
+          index = 0;
+        }
+        turn = PlayerId[index];
+      }
+    });
+    
   }
 }
 
@@ -89,28 +133,28 @@ const Blue_leftPos = ['418', '502', '418', '502'];
 function createGame(){
   for (let i=0; i<Red_bottomPos.length; i++){
     if (PlayerId.includes(0)){
-      const obj1 = new playGame('#800000', Red_bottomPos[i], Red_leftPos[i]);
+      const obj1 = new playGame('#800000', Red_bottomPos[i], Red_leftPos[i], "red");
       obj1.createToken();
       obj1.rollDice();
       obj1.moveToken();
     }
   
     if (PlayerId.includes(1)){
-      const obj2 = new playGame('#006400', Green_bottomPos[i], Green_leftPos[i]);
+      const obj2 = new playGame('#006400', Green_bottomPos[i], Green_leftPos[i], "green");
       obj2.createToken();
       obj2.rollDice();
       obj2.moveToken();
     }
     
     if (PlayerId.includes(2)){
-      const obj3 = new playGame('#FF8C00', Yellow_bottomPos[i], Yellow_leftPos[i]);
+      const obj3 = new playGame('#FF8C00', Yellow_bottomPos[i], Yellow_leftPos[i], "yellow");
       obj3.createToken();
       obj3.rollDice();
       obj3.moveToken();
     }
    
     if (PlayerId.includes(3)){
-      const obj4 = new playGame('#800080', Blue_bottomPos[i], Blue_leftPos[i]);
+      const obj4 = new playGame('#800080', Blue_bottomPos[i], Blue_leftPos[i], "blue");
       obj4.createToken();
       obj4.rollDice();
       obj4.moveToken();
